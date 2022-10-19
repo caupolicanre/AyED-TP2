89a5276316bdcbcac11589a6256854cc57148410
Caupolicán Ré <caupolicanre@gmail.com>
2022-10-19 19:24:37 -0300

    Properties, Docstrings, Getters

--- Ejercicio1/modulos/paciente.py
+++ Ejercicio1/modulos/paciente.py
@@ -4,15 +4,18 @@ from random import randint, choices
 nombres = ['Leandro', 'Mariela', 'Gastón', 'Andrea', 'Antonio', 'Estela', 'Jorge', 'Agustina']
 apellidos = ['Perez', 'Colman', 'Rodriguez', 'Juarez', 'García', 'Belgrano', 'Mendez', 'Lopez']
 
+# Riesgo y Descripción
 niveles_de_riesgo = [1, 2, 3]
 descripciones_de_riesgo = ['crítico', 'moderado', 'bajo']
 
-# probabilidades de aparición de cada tipo de paciente
+# Probabilidades de aparición de cada tipo de paciente
 probabilidades = [0.1, 0.3, 0.6] 
 
 class Paciente:
     def __init__(self, turno):
-        n = len(nombres)
+        
+        n = len(nombres)    # Variable auxiliar para asignar el nombre y apellido
+        
         self.__nombre = nombres[randint(0, n-1)]
         self.__apellido = apellidos[randint(0, n-1)]
         self.__riesgo = choices(niveles_de_riesgo, probabilidades)[0]
