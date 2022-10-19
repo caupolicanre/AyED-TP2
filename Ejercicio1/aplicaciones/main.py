1a067da98f3a1ab4fe313a70ef4f86d61122840e
Caupolicán Ré <caupolicanre@gmail.com>
2022-10-19 02:15:25 -0300

    Implemento montículo en la simulación de la sala de espera

--- Ejercicio1/aplicaciones/main.py
+++ Ejercicio1/aplicaciones/main.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 """
 Sala de emergencias
 """
@@ -6,14 +6,16 @@ Sala de emergencias
 import time
 import datetime
 import modulos.paciente as pac
+import modulos.monticulo_binario as mon
 import random
 
 n = 20  # cantidad de ciclos de simulación
 
-cola_de_espera = list()
+cola_de_espera = mon.MonticuloBinario()
 
 # Ciclo que gestiona la simulación
 for i in range(n):
+    
     # Fecha y hora de entrada de un paciente
     ahora = datetime.datetime.now()
     fecha_y_hora = ahora.strftime('%d/%m/%Y %H:%M:%S')
@@ -22,18 +24,18 @@ for i in range(n):
 
     # Se crea un paciente un paciente por segundo
     # La criticidad del paciente es aleatoria
-    paciente = pac.Paciente()
-    cola_de_espera.append(paciente)
+    paciente = pac.Paciente(i+1)  # Se inicializa con un turno de llegada
+    cola_de_espera.insertar(paciente)
 
     # Atención de paciente en este ciclo: en el 50% de los casos
     if random.random() < 0.5:
-        # se atiende paciente que se encuentra al frente de la cola
-        paciente_atendido = cola_de_espera.pop(0)
+        ''' Se atiende paciente que se encuentra al frente de la cola '''
+        paciente_atendido = cola_de_espera.eliminar_min()
         print('*'*40)
         print('Se atiende el paciente:', paciente_atendido)
         print('*'*40)
     else:
-        # se continúa atendiendo paciente de ciclo anterior
+        ''' Se continúa atendiendo paciente de ciclo anterior '''
         pass
     
     print()
