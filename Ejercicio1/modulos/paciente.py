54653a6af07d0b949e6018d3b69a7dd9322ecd06
caupolicanre <caupolicanre@gmail.com>
2022-10-19 21:42:21 -0300

    Agrego Properties del turno y hora de ingreso, Getters de las Properties, y Docstrings

--- Ejercicio1/modulos/paciente.py
+++ Ejercicio1/modulos/paciente.py
@@ -12,7 +12,7 @@ descripciones_de_riesgo = ['crítico', 'moderado', 'bajo']
 probabilidades = [0.1, 0.3, 0.6] 
 
 class Paciente:
-    def __init__(self, turno):
+    def __init__(self, turno: int, hora):
         
         n = len(nombres)    # Variable auxiliar para asignar el nombre y apellido
         
@@ -21,21 +21,54 @@ class Paciente:
         self.__riesgo = choices(niveles_de_riesgo, probabilidades)[0]
         self.__descripcion = descripciones_de_riesgo[self.__riesgo-1]
         self.__turno = turno
+        self.__hora = hora      # Hora de ingreso del Paciente a la Sala de espera
+        
 
 
     # Métodos Mágicos   
 
     def __lt__(self, other):
+        '''
+        Cuando se comparan los Pacientes primero realiza la 
+        comparación del riesgo, y si éste es igual, compara 
+        el turno del Paciente.
+
+        Parameters
+        ----------
+        other : class
+            Paciente a comparar con el actual.
+
+        Returns
+        -------
+        bool
+            Si el Paciente actual tiene un riesgo más elevado
+            que el otro, retorna True.
+            Si ambos Pacientes tienen el mismo riesgo, pero
+            el actual tiene un turno anterior, retorna True.
+
+        '''
         if self.__riesgo == other.__riesgo:
             return self.__turno < other.__turno
         return self.__riesgo < other.__riesgo
     
     
     def __str__(self):
+        '''
+        Concatena toda la información del Paciente en un
+        string y lo retorna.
+
+        Returns
+        -------
+        cad : str
+            String con el Nombre y Apellido del paciente, 
+            y su información de ingreso a la sala de espera.
+
+        '''
         cad = self.__nombre + ' '
-        cad += self.__apellido + '\t\t -> '
+        cad += self.__apellido + '\t -> '
         cad += str(self.__riesgo) + '-' + self.__descripcion
         cad += ' - Turno: ' + str(self.__turno)
+        cad += ' - ' + str(self.__hora)
         return cad
         
         
@@ -54,23 +87,6 @@ class Paciente:
         '''
         return self.__nombre
     
-    @nombre.setter
-    def nombre(self, nuevoNombre: str):
-        '''
-        Setter de Nombre.
-
-        Parameters
-        ----------
-        nuevoNombre : str
-            Nombre que va a reemplazar el antiguo Nombre del Paciente.
-
-        Returns
-        -------
-        None.
-
-        '''
-        self.__nombre = nuevoNombre
-    
     @property
     def apellido(self):
         '''
@@ -83,23 +99,6 @@ class Paciente:
 
         '''
         return self.__apellido
-    
-    @apellido.setter
-    def apellido(self, nuevoApellido: str):
-        '''
-        Setter de Apellido.
-
-        Parameters
-        ----------
-        nuevoApellido : str
-            Apellido que va a reemplazar el antiguo Apellido del Paciente.
-
-        Returns
-        -------
-        None.
-
-        '''
-        self.__apellido = nuevoApellido
     
     @property
     def riesgo(self):
@@ -122,8 +121,34 @@ class Paciente:
         Returns
         -------
         str
-            Retorna una descripción del nivel de Riesgo.
+            Retorna una descripción del nivel de Riesgo del Paciente.
 
         '''
         return self.__descripcion
     
+    @property
+    def turno(self):
+        '''
+        Getter de Turno.
+
+        Returns
+        -------
+        int
+            Retorna el número de Turno del Paciente.
+
+        '''
+        return self.__turno
+    
+    @property
+    def hora_ingreso(self):
+        '''
+        Getter de Hora de Ingreso.
+
+        Returns
+        -------
+        str
+            String de la hora de ingreso del Paciente a la 
+            Sala de Espera.
+
+        '''
+        return self.__hora
