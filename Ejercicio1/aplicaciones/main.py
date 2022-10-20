7516557c12e461e22d381391bc5c65abe15206ae
caupolicanre <caupolicanre@gmail.com>
2022-10-19 21:34:52 -0300

    Inicializo el Paciente con un turno y su hora de llegada. Comentarios

--- Ejercicio1/aplicaciones/main.py
+++ Ejercicio1/aplicaciones/main.py
@@ -5,8 +5,8 @@ Sala de emergencias
 
 import time
 import datetime
-import Ejercicio1.modulos.paciente as pac
-import Ejercicio1.modulos.monticulo_binario as mon
+import modulos.paciente as pac
+import modulos.monticulo_binario as mon
 import random
 
 n = 20  # cantidad de ciclos de simulación
@@ -19,21 +19,21 @@ for i in range(n):
     # Fecha y hora de entrada de un paciente
     ahora = datetime.datetime.now()
     fecha_y_hora = ahora.strftime('%d/%m/%Y %H:%M:%S')
-    print('-*-'*15)
+    print('-*-'*25)
     print('\n', fecha_y_hora, '\n')
 
     # Se crea un paciente un paciente por segundo
     # La criticidad del paciente es aleatoria
-    paciente = pac.Paciente(i+1)  # Se inicializa con un turno de llegada y se registra la hora de ingreso
+    paciente = pac.Paciente(turno = i+1, hora = ahora.strftime('%H:%M:%S'))  # Se inicializa con un turno de llegada y se registra la hora de ingreso
     cola_de_espera.insertar(paciente)
 
     # Atención de paciente en este ciclo: en el 50% de los casos
     if random.random() < 0.5:
         ''' Se atiende paciente que se encuentra al frente de la cola '''
         paciente_atendido = cola_de_espera.eliminar_min()
-        print('*'*40)
-        print('Se atiende el paciente:', paciente_atendido)
-        print('*'*40)
+        print('*'*80)
+        print(' Se atiende el paciente:', paciente_atendido)
+        print('*'*80)
     else:
         ''' Se continúa atendiendo paciente de ciclo anterior '''
         pass
@@ -46,7 +46,7 @@ for i in range(n):
         print('\t', paciente)
     
     print()
-    print('-*-'*15)
+    print('-*-'*25)
     
     time.sleep(1)
 
