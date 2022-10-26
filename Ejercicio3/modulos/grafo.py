f38e930974908dc367972cc4c72466f18322cba2
caupolicanre <caupolicanre@gmail.com>
2022-10-26 15:32:40 -0300

    Implementación Grafo

--- Ejercicio3/modulos/grafo.py
+++ Ejercicio3/modulos/grafo.py
@@ -1,7 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct 26 14:12:20 2022
 
-@author: Usuario
-"""
+class Vertice:
+    def __init__(self, clave):
+        self.id = clave
+        self.conectadoA = {}
 
+    def agregarVecino(self, vecino, ponderacion=0):
+        self.conectadoA[vecino] = ponderacion
+
+    def __str__(self):
+        return str(self.id) + ' conectadoA: ' + str([x.id for x in self.conectadoA])
+
+    def obtenerConexiones(self):
+        return self.conectadoA.keys()
+
+    def obtenerId(self):
+        return self.id
+
+    def obtenerPonderacion(self, vecino):
+        return self.conectadoA[vecino]
+    
+
+class Grafo:
+    def __init__(self):
+        self.listaVertices = {}
+        self.numVertices = 0
+
+    def agregarVertice(self, clave):
+        self.numVertices = self.numVertices + 1
+        nuevoVertice = Vertice(clave)
+        self.listaVertices[clave] = nuevoVertice
+        return nuevoVertice
+
+    def obtenerVertice(self, n):
+        if n in self.listaVertices:
+            return self.listaVertices[n]
+        else:
+            return None
+
+    def __contains__(self, n):
+        return n in self.listaVertices
+
+    def agregarArista(self, de, a, costo=0):
+        if de not in self.listaVertices:
+            nv = self.agregarVertice(de)
+        if a not in self.listaVertices:
+            nv = self.agregarVertice(a)
+        self.listaVertices[de].agregarVecino(self.listaVertices[a], costo)
+
+    def obtenerVertices(self):
+        return self.listaVertices.keys()
+
+    def __iter__(self):
+        return iter(self.listaVertices.values())
