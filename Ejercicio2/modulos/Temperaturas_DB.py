255d06c1bcc4dc33e94e0ad02bb04b2bbeb0b0c8
caupolicanre <caupolicanre@gmail.com>
2022-10-31 17:56:31 -0300

    Cambios menores

--- Ejercicio2/modulos/Temperaturas_DB.py
+++ Ejercicio2/modulos/Temperaturas_DB.py
@@ -6,8 +6,8 @@ class Temperaturas_DB:
     def __init__(self):
         self.arbol = AVL()
     
-    def trans_fecha(self,fecha):
-        fecha=datetime.strptime(fecha, '%d/%m/%Y')
+    def transformar_en_fecha(self, fecha):
+        fecha = datetime.strptime(fecha, '%d/%m/%Y')
         return fecha
     
     def guardar_temperatura(self, temperatura, fecha):
@@ -16,15 +16,15 @@ class Temperaturas_DB:
     def devolver_temperatura(self,fecha):
         return self.arbol.obtener(fecha)
     
-    def max_temp_rango(self,fecha1, fecha2):
-        date1=self.trans_fecha(fecha1)
-        date2=self.trans_fecha(fecha2)
+    def max_temp_rango(self, fecha1, fecha2):
+        date1 = self.trans_fecha(fecha1)
+        date2 = self.trans_fecha(fecha2)
         max_temp = 0
-        iterador=self.Iterador(self.arbol,date1)
+        iterador = self.Iterador(self.arbol, date1)
         for nodo in iterador:
-            if date1<=nodo<=date2:
-                if self.obtener(nodo)>max_temp:
-                    max_temp= self.obtener(nodo)
+            if date1 <= nodo <= date2:
+                if self.obtener(nodo) > max_temp:
+                    max_temp = self.obtener(nodo)
         return max_temp
                     
         
