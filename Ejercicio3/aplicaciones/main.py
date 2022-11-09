e5cb670500c86e24994717aa13126699e0f2e051
caupolicanre <caupolicanre@gmail.com>
2022-11-08 22:24:13 -0300

    Mejoro la aplicación para evaluar opciones de transporte
    muestro con un un mejor formato la ruta a seguir

--- Ejercicio3/aplicaciones/main.py
+++ Ejercicio3/aplicaciones/main.py
@@ -1,14 +1,28 @@
 '''
-AplicaciÃ³n para evaluar opciones de transporte
+Aplicación para evaluar opciones de transporte
 '''
 
 from procesamiento import evaluar_opciones_de_transporte
 
-ciudadInicial = input("Ingrese la ciudad inicial: ")
+ciudadInicial = input("\nIngrese la ciudad inicial: ")
 ciudadDestino = input("Ingrese la ciudad destino: ")
 
 mayor_cuello_de_botella, menor_precio, ruta = evaluar_opciones_de_transporte(ciudadInicial, ciudadDestino)
 
-print(f"\nPara ir de {ciudadInicial} a {ciudadDestino}, seguir esta ruta:\n{ruta}")
-print(f"Mayor capacidad de carga: {mayor_cuello_de_botella}")
+
+print(f"\nPara ir de {ciudadInicial} a {ciudadDestino}, seguir esta ruta:")
+
+cantidad_ciudades = len(ruta)
+ciudades_recorridas = 0 # Contador utilizado para el formato de mostrar las ciudades
+
+# Recorro la lista de ciudades de la ruta, para mostrar el recorrido
+for ciudad in ruta:
+    if ciudades_recorridas < cantidad_ciudades-1:
+        print(f"{ciudad} -> ", end="")
+        ciudades_recorridas += 1
+    else:   # Si es la última ciudad, no se escribe la flecha
+        print(f"{ciudad}")
+
+
+print(f"\nMayor capacidad de carga: {mayor_cuello_de_botella}")
 print(f"Menor precio: {menor_precio}")
